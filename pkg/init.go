111884f1ce50e7a16872c86662f70018c9260360
Manuel Odendahl <wesen@ruinwesen.com>
2023-02-07 23:48:06 -0500

    :art: Initial commit

---
+++ pkg/init.go
@@ -0,0 +1,129 @@
+package pkg
+
+import (
+	"fmt"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"gopkg.in/natefinch/lumberjack.v2"
+	"io"
+	"os"
+	"strings"
+)
+
+type LogConfig struct {
+	WithCaller bool
+	Level      string
+	LogFormat  string
+	LogFile    string
+}
+
+func InitLoggerWithConfig(config *LogConfig) error {
+	if config.WithCaller {
+		log.Logger = log.With().Caller().Logger()
+	}
+	// default is json
+	var logWriter io.Writer
+	if config.LogFormat == "text" {
+		logWriter = zerolog.ConsoleWriter{Out: os.Stderr}
+	} else {
+		logWriter = os.Stderr
+	}
+
+	if config.LogFile != "" {
+		logWriter = io.MultiWriter(
+			logWriter,
+			zerolog.ConsoleWriter{
+				NoColor: true,
+				Out: &lumberjack.Logger{
+					Filename:   config.LogFile,
+					MaxSize:    10, // megabytes
+					MaxBackups: 3,
+					MaxAge:     28,    //days
+					Compress:   false, // disabled by default
+				},
+			})
+	}
+
+	log.Logger = log.Output(logWriter)
+
+	switch config.Level {
+	case "debug":
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	case "info":
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	case "warn":
+		zerolog.SetGlobalLevel(zerolog.WarnLevel)
+	case "error":
+		zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+	case "fatal":
+		zerolog.SetGlobalLevel(zerolog.FatalLevel)
+	}
+
+	return nil
+}
+
+func InitLogger() error {
+	logLevel := viper.GetString("log-level")
+	verbose := viper.GetBool("verbose")
+	if verbose && logLevel != "trace" {
+		logLevel = "debug"
+	}
+
+	return InitLoggerWithConfig(&LogConfig{
+		Level:      logLevel,
+		LogFile:    viper.GetString("log-file"),
+		LogFormat:  viper.GetString("log-format"),
+		WithCaller: viper.GetBool("with-caller"),
+	})
+}
+
+func InitViper(appName string, rootCmd *cobra.Command) error {
+	rootCmd.PersistentFlags().String("config", "",
+		fmt.Sprintf("Path to config file (default ~/.%s/config.yml)", appName))
+
+	// parse the flags one time just to catch --config
+	configFile := ""
+	for idx, arg := range os.Args {
+		if arg == "--config" {
+			if len(os.Args) > idx+1 {
+				configFile = os.Args[idx+1]
+			}
+		}
+	}
+	viper.SetEnvPrefix(appName)
+
+	if configFile != "" {
+		viper.SetConfigFile(configFile)
+	} else {
+		viper.AddConfigPath(".")
+		viper.AddConfigPath(fmt.Sprintf("$HOME/.%s", appName))
+		viper.AddConfigPath(fmt.Sprintf("/etc/%s", appName))
+
+		xdgConfigPath, err := os.UserConfigDir()
+		if err == nil {
+			viper.AddConfigPath(fmt.Sprintf("%s/%s", xdgConfigPath, appName))
+		}
+	}
+
+	// Read the configuration file into Viper
+	err := viper.ReadInConfig()
+	// if the file does not exist, continue normally
+	if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+		// Config file not found; ignore error
+	} else if err != nil {
+		// Config file was found but another error was produced
+		return err
+	}
+	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
+	viper.AutomaticEnv()
+
+	// Bind the variables to the command-line flags
+	err = viper.BindPFlags(rootCmd.PersistentFlags())
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
