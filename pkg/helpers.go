7b71a27205a1e8c1b8d9fed6c02f661c07facde0
Manuel Odendahl <wesen@ruinwesen.com>
2023-02-11 22:12:43 -0500

    :art: Add more casting helpers

--- pkg/helpers.go
+++ pkg/helpers.go
@@ -15,3 +15,33 @@ func CastList[To any, From any](list []From) ([]To, bool) {
 
 	return ret, true
 }
+
+func CastStringMap[To any, From any](m map[string]From) (map[string]To, bool) {
+	ret := map[string]To{}
+
+	for k, v := range m {
+		var item interface{} = v
+		casted, ok := item.(To)
+		if !ok {
+			return ret, false
+		}
+
+		ret[k] = casted
+	}
+
+	return ret, true
+}
+
+func CastMapMember[To any](m map[string]interface{}, k string) (*To, bool) {
+	v, ok := m[k]
+	if !ok {
+		return nil, false
+	}
+
+	casted, ok := v.(To)
+	if !ok {
+		return nil, false
+	}
+
+	return &casted, true
+}
